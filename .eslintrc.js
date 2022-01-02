624196326731ae6124caefa9748d1d004e4416cc
Keita Katahira <katahirakeita1201@gmail.com>
2022-01-03 01:15:48 +0900

    [Add] eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {},
+  ignorePatterns: ['/dist/**']
+};
