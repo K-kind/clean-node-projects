6a472a7e9126486cd1b7d503f9b19451b379e334
Keita Katahira <katahirakeita1201@gmail.com>
2022-01-06 00:00:41 +0900

    [Refactor] help text

--- src/cli.ts
+++ src/cli.ts
@@ -3,22 +3,7 @@ import { hideBin } from 'yargs/helpers';
 import { Cleaner } from './lib/cleaner';
 
 export const cli = async (rawArgv: string[]) => {
-  const argv = yargs(hideBin(rawArgv))
-    .usage('Usage: $0 [path] [options]')
-    .option('target', {
-      alias: 't',
-      description: 'Folder names to remove',
-      type: 'array',
-      default: Cleaner.DEFAULT_TARGETS
-    })
-    .option('fast', {
-      alias: 'f',
-      description: 'Do not calculate size',
-      type: 'boolean'
-    })
-    .help()
-    .locale('en')
-    .parseSync();
+  const argv = parseArgs(rawArgv);
 
   const cleaner = new Cleaner({
     rootPathArg: argv._[0] as string,
@@ -27,4 +12,35 @@ export const cli = async (rawArgv: string[]) => {
   });
 
   await cleaner.execute();
+};
+
+const parseArgs = (rawArgv: string[]) => {
+  const slicedArgs = hideBin(rawArgv);
+  return yargs(slicedArgs)
+    .usage('Usage: $0 [path] [options]')
+    .option('target', {
+      alias: 't',
+      description: 'Folder names to find',
+      type: 'array',
+      default: Cleaner.DEFAULT_TARGETS
+    })
+    .option('fast', {
+      alias: 'f',
+      description: 'Do not calculate size',
+      type: 'boolean'
+    })
+    .example(
+      '$0',
+      `Find ${Cleaner.DEFAULT_TARGETS.join(
+        ', '
+      )} folders under the current path and remove selected ones.`
+    )
+    .example(
+      '$0 ~/Desktop --target node_modules build',
+      `Find node_modules and build folders under the Desktop and remove selected ones.`
+    )
+    .help()
+    .locale('en')
+    .wrap(90)
+    .parseSync();
 };
