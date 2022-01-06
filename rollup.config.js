aa080c8e9eebfe85eeecd8b8f27822e5e56d3aaf
Keita Katahira <katahirakeita1201@gmail.com>
2022-01-06 22:20:07 +0900

    [Update] change rollup plugins by NODE_ENV

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,16 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import packageJson from './package.json';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const globals = {
   ...packageJson.dependencies
 };
+
+const plugins = [
+  ...(isProd ? [commonjs(), nodeResolve(), terser()] : []),
+  typescript()
+];
 
 export default {
   input: 'src/cli.ts',
@@ -16,6 +23,6 @@ export default {
       format: 'cjs'
     }
   ],
-  plugins: [commonjs(), nodeResolve(), terser(), typescript()],
+  plugins,
   external: Object.keys(globals)
 };
